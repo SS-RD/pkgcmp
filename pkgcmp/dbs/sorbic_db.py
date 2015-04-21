4baeb096bbc596364d54f6c6bdf0c671b846a4bc
Thomas S Hatch <thatch45@gmail.com>
2015-04-20 22:54:54 -0600

    just put the sorbic db in the cachedir

--- pkgcmp/dbs/sorbic_db.py
+++ pkgcmp/dbs/sorbic_db.py
@@ -1,14 +1,22 @@
 '''
 Store data in a sorbic database
 '''
+# Import python libs
+import os
 # Import sorbic libs
 import sorbic.db
+
+__virtualname__ = 'sorbic'
+
+
+def __virtual__():
+    return __virtualname__
 
 
 def save_pkg(pkg):
     '''
     Save the data for the given package
     '''
-    db = sorbic.db.DB(__opts__['sorbic']['root'])
+    db = sorbic.db.DB(os.path.join(__opts__['cachedir'], 'sorbic'))
     key = '{}/{}'.format(pkg['distro'], pkg['name'])
     db.insert(key, pkg)
