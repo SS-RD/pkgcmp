1d5198d1ce4d6ed993f6cb13e4ae93dd33a322f5
Thomas S Hatch <thatch45@gmail.com>
2015-04-20 20:49:38 -0600

    Add basic cli and config parsing

---
+++ pkgcmp/cli.py
@@ -0,0 +1,42 @@
+'''
+Parse CLI options
+'''
+# Import python libs
+import os
+import copy
+import argparse
+# Import third party libs
+import yaml
+
+DEFAULTS = {}
+
+
+def parse():
+    '''
+    Parse!!
+    '''
+    parser = argparse.ArgumentParser(description='The pkgcmp map generator')
+    parser.add_argument(
+            '--config',
+            dest='config',
+            default='/etc/pkgcmp/pkgcmp',
+            help='The location of the pkgcmp config file')
+    opts = parser.parse_args().__dict__
+    conf = config(opts['config'])
+    for key in opts:
+        if opts[key] is not None:
+            conf[key] = opts[key]
+    return conf
+
+
+def config(cfn):
+    '''
+    Read in the config file
+    '''
+    ret = copy.copy(DEFAULTS)
+    if os.path.isfile(cfn):
+        with open(cfn, 'r') as cfp:
+            conf = yaml.safe_load(cfp)
+            print(conf)
+            ret.update(conf)
+    return ret
